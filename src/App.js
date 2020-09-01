cd72cd52429547b52e0bee2da29559656a3df954
Daniel Arango <arangomonsalve.daniel@gmail.com>
2020-09-01 09:51:22 -0500

    Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
+import Widgets from "./components/Widgets";
+import Login from "./components/Login";
+import {useStateValue} from "./StateProvider";
 
 function App() {
+
+    const [{isLoggedIn}, dispatch] = useStateValue();
+
     return (
         <div className="app">
-            <Header/>
+            {isLoggedIn
+                ?
+                <>
+                    <Header/>
+                    <div className="app__body">
+                        <Sidebar/>
+                        <Feed/>
+                        <Widgets/>
+                    </div>
+                </>
+                :
+                <Login/>
+            }
 
-            <div className="app__body">
-                <Sidebar/>
-                <Feed/>
-            </div>
         </div>
     );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDfY7Uw_FyOZTBrkcmuxLcKulNle9xu3pc",
     authDomain: "facebook-clone123.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:919048438783:web:14da19e5d3f99e3b23ddca",
     measurementId: "G-4NTV9V4YH1"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore()
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider}
+export default db;
